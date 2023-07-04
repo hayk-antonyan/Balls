bf1a2f9fb817ce45212a6d969ba3d7a516138dd7
Hayk_Antonyan <30365848+hayk-antonyan@users.noreply.github.com>
2023-07-04 17:29:22 +0400

    Update balls.js

--- balls.js
+++ balls.js
@@ -1,2 +1,8 @@
 //console.log(5);
-Hello
+var getName= prompt("What is your name?");
+var firstChar= getName.slice(0,1);
+var firstCharUpper= firstChar.toUpperCase();
+var restOfName= getName.slice(1, getName.length);
+restOfName=restOfName.toLowerCase();
+var fullName=firstCharUpper+restOfName;
+alert(fullName);
