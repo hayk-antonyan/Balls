97963439315fdc115278c6b5b6853b095fdcbb8a
Hayk_Antonyan <30365848+hayk-antonyan@users.noreply.github.com>
2023-10-18 22:42:47 +0400

    update 18.10.23

--- balls.js
+++ balls.js
@@ -37,31 +37,85 @@ function creatBoardView(){
     printingDiv.classList.add("grey");
     containerElement.appendChild(printingDiv);
     printingDiv.setAttribute('id', i);
+    // add setattribute style - (add width to a div in js -> check how to)
   }
 };
 
+// function updateBoardVIew() {
+//   console.log(board);
+//   for (let i = 0; i < board.length; i++) {
+//       const cell = document.getElementById(i);
+//       const element = board[i];
+
+//       if (element) {
+//           const color = colors[element.colorIndex];
+          
+//           // Create a new colored div element
+//           const ballDiv = document.createElement("div");
+//           ballDiv.classList.add("ball", color);
+          
+//           // Remove any existing child elements and append the new colored div
+//           while (cell.firstChild) {
+//               cell.removeChild(cell.firstChild);
+//           }
+//           cell.appendChild(ballDiv);
+
+//           if (element.isActive) {
+//               cell.classList.add('active-ball');
+//           } else {
+//               cell.classList.remove('active-ball');
+//           }
+
+//       } else {
+//           cell.classList.remove(...cell.classList);
+//           cell.classList.add('grey');
+//       }
+//   }
+// }
+
+
 function updateBoardVIew(){
-    
+  // console.log(board);
   for (let i = 0; i < board.length; i++){
     const cell = document.getElementById(i);
-     const element = board[i];
+    const element = board[i];
+
     if( element){
       const color =colors[element.colorIndex];
       cell.classList.add('ball', color);
+
       if(element.isActive){
-      cell.classList.add('active-ball');
+        cell.classList.add('active-ball');
+      }else{
+        cell.classList.remove('active-ball');
       }
-    else{
-      cell.classList.remove("active-ball");
+
+    }else{
+        cell.classList.remove(...cell.classList);
+        cell.classList.add('grey');
     }
-  }
   }
 }
 
-function getRandomBoardIndex(){
 
-  const randomCellIndex = Math.floor(Math.random() * board.length);
-  return randomCellIndex;
+// -------------- Get empty cells ----------------------------------------
+
+function getEmptyCells() {
+  const emptyCells = [];
+  for (let i = 0; i < board.length; i++) {
+    if (!board[i]) {
+      emptyCells.push(i);
+    }
+  }
+  return emptyCells;
+  // console.log(emptyCells);    
+} 
+
+
+function getRandomBoardIndex(emptyCells){
+
+  const randomCellIndex = Math.floor(Math.random() * emptyCells.length);
+  return emptyCells[randomCellIndex];
   alert("This is randomcell index " + randomCellIndex);
 }
 
@@ -75,17 +129,24 @@ function getRandomColorIndex(){
 function addRandomBalls() {
 
   for (let i = 0; i < 3; i++) {
-
-    const randomBoardIndex = getRandomBoardIndex(); 
+    const emptyCells =  getEmptyCells();
+    const randomBoardIndex = getRandomBoardIndex(emptyCells); 
     const randomColor = getRandomColorIndex();
     board[randomBoardIndex] = {colorIndex: randomColor, isActive: false};
   }
 }
 
+// function printBoard() {
+
+// }
+
+// // Use setTimeout to call printBoard after a delay of 1000 milliseconds (1 second)
+// setTimeout(printBoard, 3000);
+
 // -------------------------------------------------------------------------
 
 function handleCellClick(e, i){
-  console.log(e);
+  // console.log(e);
 
 const activeBallIndex = board.findIndex(element => element !== null && element.isActive);
 
@@ -94,14 +155,39 @@ const activeBallIndex = board.findIndex(element => element !== null && element.i
     return;
   }
 // case 2: if board i is empty and activeBall is not null => moveBalls, addRandomBalls, updateBoardView
-  if( !board[i] && activeBallIndex >= 0){
-    moveBalls(activeBallIndex, i);
-    addRandomBalls();
-    // նոր տեղ տեղափոխված գնդակը պետքա active չլինի 
-    removeBall();
-    updateBoardVIew();
-    return;
+
+
+if (!board[i] && activeBallIndex >= 0) {
+  moveBalls(activeBallIndex, i);
+  const ballsRemoved = removeMatchingBalls();
+  // If balls were removed, add new random balls
+  if (ballsRemoved > 0) {
+      let count = 0;
+      updateBoardVIew();
+      count ++;
+      console.log(ballsRemoved);
+      // alert(count);
+  } else {
+      addRandomBalls();
+      updateBoardVIew();
   }
+  return;
+}
+
+
+  // if( !board[i] && activeBallIndex >= 0){
+  //   moveBalls(activeBallIndex, i);
+  //   ballsRemoved = removeMatchingBalls();
+  //   // If no balls were removed, add new random balls
+  // if (!ballsRemoved) {
+  //     addRandomBalls();
+  //     updateBoardVIew();
+  //   }
+  //   // while (removeMatchingBalls()) {} // Remove all matching balls until there are no more matches
+  //   updateBoardVIew();
+  //   return;
+  // }
+
 // case 3: if board i is not empty and activeBall is null => activeBall
   if( board[i] && activeBallIndex === -1){
     activeBall(i);   
@@ -109,62 +195,43 @@ const activeBallIndex = board.findIndex(element => element !== null && element.i
     // console.log("This part is updated" + i);
     return;
   }
-// case 4: if board i is not empty and activeBall is not null, make clicked ball activ and hasActive null;
+// case 4: if board i is not empty and activeBall is not null, make clicked ball activ and TURN ACTIVE BALL PASSIVE;
   if( board[i] && activeBallIndex >= 0){
-    activeBall(i); //HERE ACTIVE BALL DOESNT RETURN PASSIVE
+
+    activeBall(i); //HERE ACTIVE BALL DOESNT TURN PASSIVE
     updateBoardVIew();
     return;
-  } console.log(activeBallIndex);
+  } 
+  
 } 
 
 // ------------------------------------------------------------------------
 
-// function activeBall(cellIndex) {
-//     const cell = board[cellIndex];
-    
-//     if (cell && !cell.isActive) {
-//       cell.isActive = true;    
-//       return;
-//     } 
-  
-//     if (cell && cell.isActive) {
-//       cell.isActive = false;
-//       return;
-//     }
-// }
-
 function activeBall(cellIndex) {
-    const cell = board[cellIndex];
-    
-    if (cell) {
-        cell.isActive = !cell.isActive;
-        
-        // If you want to deactivate all other active cells when activating a new one
-        if (cell.isActive) {
-            for (let i = 0; i < board.length; i++) {
-                if (board[i] && board[i].isActive && i !== cellIndex) {
-                    board[i].isActive = false;
-                }
-            }
-        }
-    }
-}
+  // Deactivate all active cells
 
-function removeBall() {
-    for (let i = 0; i < board.length; i++) {
-        const cell = board[i];
-        if (cell && cell.isActive) {
-          cell.isActive = false
-          // board[i] = null;
-        }
-    }
+  for (let i = 0; i < board.length; i++) {
+      if (board[i] && board[i].isActive) {
+          board[i].isActive = false;
+      }
+  }
+
+  const cell = board[cellIndex];
+  
+  if (cell) {
+      // Activate the clicked cell
+      cell.isActive = true;
+  }
 }
 
 function moveBalls(fromIndex, toIndex) {
     if (fromIndex >= 0 && fromIndex < board.length && toIndex >= 0 && toIndex < board.length) {
         const elementToMove = board[fromIndex];
         board[fromIndex] = null; 
-        board[toIndex] = elementToMove; 
+        board[toIndex] = {
+          ...elementToMove,
+         isActive: false
+        }
         return true; 
     } else {
         // console.log("Ball is not moved.");
@@ -176,24 +243,157 @@ let fromIndex = 0;
 let toIndex = 81;
 
 if (moveBalls(fromIndex, toIndex, board)) {
-    // console.log("Move successful.");
+    console.log("Move successful.");
     // console.log("Array after move:", board);
 } else {
     // console.log("Move failed.");
 }
 
+// ------------ Removing matching balls with the same color -----------------------
+
+function removeMatchingBalls() {
+
+  const rows = 9; // Number of rows on the board
+  const cols = 9; // Number of columns on the board
+  let removed = false; // Flag to track if any balls were removed
+
+  // Check for horizontal matches
+  for (let row = 0; row < rows; row++) {
+      for (let col = 0; col < cols - 2; col++) {
+          const currentIndex = row * cols + col;
+          const currentCell = board[currentIndex];
+
+          if (currentCell) {
+              const colorIndex = currentCell.colorIndex;
+
+              // Check if the next two cells in the row have the same color
+              if (
+                  board[currentIndex + 1]?.colorIndex === colorIndex &&
+                  board[currentIndex + 2]?.colorIndex === colorIndex &&
+                  board[currentIndex + 3]?.colorIndex === colorIndex &&
+                  board[currentIndex + 4]?.colorIndex === colorIndex
+
+               ) {
+                  // Found a horizontal match of three balls of the same color
+                  board[currentIndex] = null;
+                  board[currentIndex + 1] = null;
+                  board[currentIndex + 2] = null;
+                  board[currentIndex + 3] = null;
+                  board[currentIndex + 4] = null;
+
+                  removed = true;
+              }
+          }
+      }
+  }
+
+  // Check for vertical matches
+  for (let col = 0; col < cols; col++) {
+      for (let row = 0; row < rows - 2; row++) {
+          const currentIndex = row * cols + col;
+          const currentCell = board[currentIndex];
+
+          if (currentCell) {
+              const colorIndex = currentCell.colorIndex;
+
+              // Check if the next two cells in the column have the same color
+              if (
+                  board[currentIndex + cols]?.colorIndex === colorIndex &&
+                  board[currentIndex + 2 * cols]?.colorIndex === colorIndex &&
+                  board[currentIndex + 3 * cols]?.colorIndex === colorIndex &&
+                  board[currentIndex + 4 * cols]?.colorIndex === colorIndex
+
+              ) {
+                  // Found a vertical match of three balls of the same color
+                  board[currentIndex] = null;
+                  board[currentIndex + cols] = null;
+                  board[currentIndex + 2 * cols] = null;
+                  board[currentIndex + 3 * cols] = null;
+                  board[currentIndex + 4 * cols] = null;
+
+                  removed = true;
+              }
+          }
+      }
+  }
+
+   // Check for diagonal matches (from top-left to bottom-right)
+   for (let row = 0; row < rows - 4; row++) {
+    for (let col = 0; col < cols - 4; col++) {
+        const currentIndex = row * cols + col;
+        const currentCell = board[currentIndex];
+
+        if (currentCell) {
+            const colorIndex = currentCell.colorIndex;
+
+            // Check if the next four cells diagonally have the same color
+            if (
+                board[currentIndex + cols + 1]?.colorIndex === colorIndex &&
+                board[currentIndex + 2 * cols + 2]?.colorIndex === colorIndex &&
+                board[currentIndex + 3 * cols + 3]?.colorIndex === colorIndex &&
+                board[currentIndex + 4 * cols + 4]?.colorIndex === colorIndex
+            ) {
+                // Found a diagonal match of five balls of the same color
+                for (let i = 0; i < 5; i++) {
+                    board[currentIndex + i * (cols + 1)] = null;
+                }
+                removed = true;
+            }
+        }
+    }
+}
+
+// Check for diagonal matches (from top-right to bottom-left)
+for (let row = 0; row < rows - 4; row++) {
+    for (let col = 4; col < cols; col++) {
+        const currentIndex = row * cols + col;
+        const currentCell = board[currentIndex];
+
+        if (currentCell) {
+            const colorIndex = currentCell.colorIndex;
+
+            // Check if the next four cells diagonally have the same color
+            if (
+                board[currentIndex + cols - 1]?.colorIndex === colorIndex &&
+                board[currentIndex + 2 * cols - 2]?.colorIndex === colorIndex &&
+                board[currentIndex + 3 * cols - 3]?.colorIndex === colorIndex &&
+                board[currentIndex + 4 * cols - 4]?.colorIndex === colorIndex
+            ) {
+                // Found a diagonal match of five balls of the same color
+                for (let i = 0; i < 5; i++) {
+                    board[currentIndex + i * (cols - 1)] = null;
+                }
+                removed = true;
+            }
+        }
+    }
+}
+
+  return removed;
+}
+
+function myScore(ballsRemoved){
+  let removedCount  = 0;
+  if(ballsRemoved){
+    removedCount  ++;
+  }
+  // console.log(removedCount);
+}
+
+myScore();  
+
+// for (let i = 0; i<1000; i ++){
+//   const currentScore = removeMatchingBalls;
+// } 
 
 
-
-// -------------- Get empty cells ----------------------------------------
-
-// function getEmptyCells() {
-//   const emptyCells = [];
-//   for (let i = 0; i < board.length; i++) {
-//     if (!board[i]) {
-//       emptyCells.push(i);
+// function removeBall() {
+//     for (let i = 0; i < board.length; i++) {
+//         const cell = board[i];
+//         if (cell && cell.isActive) {
+//           cell.isActive = false
+//           // board[i] = null;
+//         }
 //     }
-//   }
-//   return emptyCells;
-//   console.log(emptyCells);    
 // }
+
